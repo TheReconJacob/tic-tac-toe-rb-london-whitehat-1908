0f20f2f8dea69e53a6249d3bfdbf23a261b08dea
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-08 08:23:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,9 +38,9 @@ end
 def turn_count(board)
   counter = 0
   board.each do |token|
-  if token == "X" or token == "O"
-    counter += 1 #counter = counter
-  end
+    if token == "X" or token == "O"
+      counter += 1 #counter = counter
+    end
   counter
 end
 
